acb24f2a2fa59ce615665486cbce6b5a7bd46c1b
spiritGo <1194501891@qq.com>
2020-02-29 09:16:42 +0800

    dev

---
+++ src/common/js/utils.js
@@ -0,0 +1,76 @@
+/**
+ * 会话存储
+ * key { String } 键
+ * value { Any } 值
+ */
+const session = {
+    set(key, value) {
+        value = typeof value == 'object' ? JSON.stringify(value) + `-${typeof value}` : value + `-${typeof value}`;
+        sessionStorage.setItem(key, value)
+    },
+    get(key) {
+        let _temp = sessionStorage.getItem(key).split('-');
+        switch (_temp[1]) {
+            case 'number':
+                _temp[0] = JSON.parse(_temp[0])
+                break;
+            case 'boolean':
+                _temp[0] = JSON.parse(_temp[0])
+                break;
+            case 'object':
+                _temp[0] = JSON.parse(_temp[0])
+                break;
+            case 'undefined':
+                _temp[0] = undefined;
+                break;
+            default:
+                break;
+        }
+
+        return _temp[0]
+    },
+    remove(key) {
+        sessionStorage.removeItem(key)
+    }
+}
+
+/**
+ * 本地存储
+ * key { String } 键
+ * value { Any } 值
+ */
+const local = {
+    set(key, value) {
+        value = typeof value == 'object' ? JSON.stringify(value) + `-${typeof value}` : value + `-${typeof value}`;
+        localStorage.setItem(key, value)
+    },
+    get(key) {
+        let _temp = localStorage.getItem(key).split('-');
+        switch (_temp[1]) {
+            case 'number':
+                _temp[0] = JSON.parse(_temp[0])
+                break;
+            case 'boolean':
+                _temp[0] = JSON.parse(_temp[0])
+                break;
+            case 'object':
+                _temp[0] = JSON.parse(_temp[0])
+                break;
+            case 'undefined':
+                _temp[0] = undefined;
+                break;
+            default:
+                break;
+        }
+
+        return _temp[0]
+    },
+    remove(key) {
+        localStorage.removeItem(key)
+    }
+}
+
+export default {
+    session,
+    local
+}
--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import utils from './common/js/utils';
 
 Vue.config.productionTip = false
+
+/* 在 vue 中添加功能 */
+Vue.prototype.$utils = utils;
 
 new Vue({
   router,
